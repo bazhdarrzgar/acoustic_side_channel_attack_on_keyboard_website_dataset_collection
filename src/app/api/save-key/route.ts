80eeca8dd35a2b01d3c346360caf93395b79998c
bazhdarrzgar <soyansoon9@gmail.com>
2026-05-01 21:25:56 +0300

    feat: add function keys F1-F12 to keyMap for safe filename generation

--- src/app/api/save-key/route.ts
+++ src/app/api/save-key/route.ts
@@ -71,6 +71,9 @@ export async function POST(req: NextRequest) {
       'Shift': 'Shift',
       'Fn': 'Fn',
       'Function': 'Fn',
+      'F1': 'F1', 'F2': 'F2', 'F3': 'F3', 'F4': 'F4',
+      'F5': 'F5', 'F6': 'F6', 'F7': 'F7', 'F8': 'F8',
+      'F9': 'F9', 'F10': 'F10', 'F11': 'F11', 'F12': 'F12',
     };
 
     const safeKey = keyMap[key] || key.replace(/[^a-z0-9]/gi, '_');
